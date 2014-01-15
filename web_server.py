c13c6e5a20daca946e1a3110739d63fc265496e3
Arlan Jaska <ajaska@berkeley.edu>
2014-01-15 13:05:00 -0500

    Improve session management

--- web_server.py
+++ web_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
 import api, markov_model
 from chatterbotapi import ChatterBotFactory, ChatterBotType
+from user import User
 
 factory = ChatterBotFactory()
 
@@ -15,14 +16,11 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 #app.config.from_object('SERVER_SETTINGS')
 
-myturn = {}
-model = {}
-lastmessage= {}
-botsession = {}
+users = {}
 
 @app.route('/')
 def front_page():
-    if session.get('access_token'):
+    if session.get('id'):
         return render_template('chatbot.html', uid=session['id'])
     else:
         return render_template('landing.html')
@@ -41,9 +39,8 @@ def code_process(uniqueid):
         code = request.args.get('code')
         token = api.getToken(code, uniqueid)
         if api.verifyToken(token):
-            session['access_token'] = token
+            users[uniqueid] = User(token, uniqueid)
             session['id'] = uniqueid
-            session['name'] = api.getFirstName(token)
         return redirect(url_for('front_page'))
     elif 'error' in request.args:
         raise api.APIError(request.args.get('error_description'))
@@ -53,27 +50,7 @@ def generate_message(uniqueid):
     if not 'id' in session or session['id'] != uniqueid:
         abort(403)
     else:
-        if not uniqueid in model:
-            statuses = api.getAllStatuses(session['access_token'])
-            markov = markov_model.generateMarkovMap(statuses)
-            model[uniqueid] = lambda: markov_model.generateStatus(markov, statuses)
-        if not uniqueid in myturn:
-            myturn[uniqueid] = True
-        if not uniqueid in botsession:
-            botsession[uniqueid] = factory.create(ChatterBotType.CLEVERBOT).create_session()
-        if not uniqueid in lastmessage:
-            lastmessage[uniqueid] = 'Hi'
-
-        if myturn[uniqueid]:
-            myturn[uniqueid] = not myturn[uniqueid]
-            message = markov_model.makeSimilarStatus(lastmessage[uniqueid], model[uniqueid])
-            lastmessage[uniqueid] = message
-            return jsonify({"message":session['name']+ ": " + message})
-        else:
-            myturn[uniqueid] = not myturn[uniqueid]
-            message =  botsession[uniqueid].think(lastmessage[uniqueid])
-            lastmessage[uniqueid] = message
-            return jsonify({"message":"Cleverbot: "+  message})
+        return jsonify({"message":users[uniqueid].generateMessage()})
 
 @app.route('/about')
 def about():
