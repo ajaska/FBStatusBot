91514035a0ebec1f5fdedc0dcc1cdd0e2aec5e38
Arlan Jaska <ajaska@berkeley.edu>
2014-01-16 08:14:25 -0800

    Add basic AJAX error handling

--- static/ajax_results.js
+++ static/ajax_results.js
@@ -7,6 +7,9 @@ function getProgress(uid) {
 		success: function (msg, data) {
 			$("#log").append("<p>"+msg['message']+"</p>");
 			setTimeout(function(){getProgress(uid)},timeout);
+		},
+		error: function (jqXHR, errorStatus, errorThrown) {
+			$("#log").append("<span class='error'>An error occured, the server may be overloaded or down. Try refreshing the page.</span>");
 		}
    });
 }
